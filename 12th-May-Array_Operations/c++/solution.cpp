cc00a510706b6ba42ea6059d1fc2797e26aa8f4e
Bhaskar <115359430+bhaskaraa45@users.noreply.github.com>
2023-05-12 11:12:09 +0530

    Some changes

--- 12th-May-Array_Operations/c++/solution.cpp
+++ 12th-May-Array_Operations/c++/solution.cpp
@@ -4,8 +4,15 @@ This Code is written by Bhaskar
 
 class Solution {
   public:
+  boolean isZeroPresents(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0) return true;
+        }
+        return false;
+    }
     int arrayOperations(int n, vector<int> &arr) {
         // code here
+      if(! isZeroPresents(arr)) return -1;
         int cnt=0,temp=0;
         for(int i=0;i<n ;i++){
             if(arr[i]!=0){
